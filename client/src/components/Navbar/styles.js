c21045a32020b74fb740bf343e9a2d400f4f88f7
maksborsch <maksborsch1987@gmail.com>
2022-06-30 17:24:17 +0300

    First commit

---
+++ client/src/components/Navbar/styles.js
@@ -0,0 +1,71 @@
+import { makeStyles } from "@material-ui/core/styles";
+import { deepPurple } from "@material-ui/core/colors";
+
+export default makeStyles((theme) => ({
+  appBar: {
+    backgroundImage: ["linear-gradient(120deg, #ffffff 30%, #8fd3f4 100%)"],
+    borderRadius: 15,
+    margin: "30px 0",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: "10px 50px",
+    [theme.breakpoints.down("sm")]: {
+      flexDirection: "column",
+    },
+
+  },
+  heading: {
+    fontFamily: [
+      'Nunito',
+      "sans-serif",
+    ].join(","),
+    color: "#8aad34",
+    textDecoration: "none",
+    fontSize: "2em",
+    fontWeight: 300,
+    
+
+  },
+  image: {
+    marginLeft: "10px",
+    marginTop: "5px",
+  },
+  toolbar: {
+    display: "flex",
+    justifyContent: "flex-end",
+    width: "400px",
+    [theme.breakpoints.down("sm")]: {
+      width: "auto",
+    },
+  },
+  profile: {
+    display: "flex",
+    justifyContent: "space-between",
+    width: "400px",
+    alignItems: "center",
+    [theme.breakpoints.down("sm")]: {
+      width: "auto",
+      marginTop: 20,
+      justifyContent: "center",
+    },
+  },
+  logout: {
+    marginLeft: "20px",
+  },
+  userName: {
+    display: "flex",
+    alignItems: "center",
+    textAlign: "center",
+  },
+  brandContainer: {
+    display: "flex",
+    alignItems: "center",
+    textDecoration: "none",
+  },
+  purple: {
+    color: theme.palette.getContrastText(deepPurple[500]),
+    backgroundColor: deepPurple[500],
+  },
+}));
